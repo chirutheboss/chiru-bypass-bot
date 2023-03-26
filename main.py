fb4fa3089156071ecd90947e556e46792a30becb
chirutheboss <85287194+chirutheboss@users.noreply.github.com>
2023-03-26 14:12:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -99,6 +99,33 @@ def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_
     app.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, i am Link Bypasser Bot, just send me any supported links and i will you get you results.\nCheckout /help to Read More__",
     reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Source Code", url="https://github.com/bipinkrish/Link-Bypasser-Bot")]]), reply_to_message_id=message.id)
 
+async def handle_force_sub(bot: Client, cmd: Message):
+    try:
+        user = await bot.get_chat_member(chat_id=-1001713795228,
+                                         user_id=cmd.from_user.id)
+        if user.status in (ChatMemberStatus.BANNED,
+                           ChatMemberStatus.RESTRICTED):
+            await cmd.reply_text(
+                text=
+                "Sorry, You are Banned to use me. Contact my [Support Group](https://t.me/greymatters_bots_discussion).",
+                disable_web_page_preview=True,
+            )
+            return 0
+    except UserNotParticipant:
+        try:
+            await cmd.reply_text(
+                text="**Please Join My Updates Channel to use me!**\n\n"
+                "Due to Overload, Only Channel Subscribers can use the Bot!",
+                reply_markup=InlineKeyboardMarkup([
+                    [
+                        InlineKeyboardButton(
+                            "🤖 Join Updates Channel",
+                            url="t.me/GreyMatter_Bots",
+                        )
+                    ],
+                ]),
+            )
+            return 0
 
 # help command
 @app.on_message(filters.command(["help"]))
