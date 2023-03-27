011b632d9881799f0b767df1fb95d0f07d3817bc
chirutheboss <85287194+chirutheboss@users.noreply.github.com>
2023-03-27 22:05:29 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1209,21 +1209,13 @@ def gdtot(url: str, GdTot_Crypt: str) -> str:
     base_url = re.match('^.+?[^\/:](?=[?\/]|$\n)', url).group(0)
     res = client.get(f"{base_url}/dld?id={url.split('/')[-1]}")
     url = re.findall(r'URL=(.*?)"', res.text)[0]
-    info = {}
-    info["error"] = False
     params = parse_qs(urlparse(url).query)
     if "gd" not in params or not params["gd"] or params["gd"][0] == "false":
-        info["error"] = True
-        if "msgx" in params:
-            info["message"] = params["msgx"][0]
-        else:
-            info["message"] = "Invalid link"
+        return "Invalid link"
     else:
         decoded_id = base64.b64decode(str(params["gd"][0])).decode("utf-8")
-        drive_link = f"https://drive.google.com/open?id={decoded_id}"
-        info["gdrive_link"] = drive_link
-    if not info["error"]: return info["gdrive_link"]
-    else: return ddl.gdtot(url)
+        drive_link = f"https://drive.google.com/uc?id={decoded_id}&export=download"
+        return drive_link
 
 
 ##################################################################
